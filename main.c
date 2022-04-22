a04ce1071817a579426edcdf867ed6b7a333e943
Dmitrii Pavlov <wowislive@gmail.com>
2022-04-22 19:04:03 +0300

    ver 0.2 added functions for running the program in the terminal step by step

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define board_h 9  /* board height */
 #define board_w 9  /* board width */
+#define MAX_USER_INPUT 2000
 
 struct cell{
 int current;  /* current situation, which is visible on screen */
@@ -12,21 +15,41 @@ struct cell board [board_h] [board_w] = {0, 0};
 
 int count_neighbour_cell(int x, int y);
 int future_life_change(int i, int k);
-void board_status_exchange();
+void board_status_exchange(void);
+char ask_command(void);
+void print_current_board(void);
+void init_board_state(void);
 
 int main(void){
 
   int neighbour;
+  char input_command;
 
-  for(int i=0; i<board_h; i++){
-    for(int j=0; j<board_w; j++){
-      
-      neighbour = count_neighbour_cell(i, j);
-      board[i][j].future = future_life_change(neighbour, board[i][j].current);
+  srand (time(NULL)); 
+  init_board_state();
+  printf("N to print next step\nX to terminate programm\n");
+
+  do{
+    input_command = ask_command();
+
+    if(input_command == 'N'){
+      for(int i=0; i<board_h; i++){
+        printf("\n");
+        for(int j=0; j<board_w; j++){
+          
+          if(board[i][j].current == 0)
+            printf("O");
+          else if(board[i][j].current == 1)
+            printf("X");
+
+          neighbour = count_neighbour_cell(i, j);
+          board[i][j].future = future_life_change(neighbour, board[i][j].current);
+        }
+      }
+    printf("\n");
+    board_status_exchange();
     }
-  }
-
-  board_status_exchange();
+  }while(input_command!= 'X');
 
   return 0;
 }
@@ -41,10 +64,11 @@ int count_neighbour_cell(int h, int w){  /*returns the count of alive neighbours
       if(i==h && j==w)
         continue;
       
-      if(i<h && j<w && i>=0 && j>=0)
+      if((i<board_h) && (j<board_w) && (i>=0) && (j>=0))
         count += board[i][j].current;
     }
   }
+  
   return count;
 }
 
@@ -61,10 +85,32 @@ int future_life_change(int i, int k){  /*changes the life status of a cell depen
   return life_state;
 }
 
-void board_status_exchange(){  /*assigns the values of the future board to the current board*/
+void board_status_exchange(void){  /*assigns the values of the future board to the current board*/
   for(int i=0; i<board_h; i++){
     for(int j=0; j<board_w; j++)
       
       board[i][j].current = board[i][j].future;
   }
+}
+
+char ask_command(void){  /*Ask char from user*/
+
+  char s[MAX_USER_INPUT];
+  //printf("\nEnter command: ");
+
+  do{
+    fgets(s,MAX_USER_INPUT,stdin);
+
+    if(s[0] > 96 && s[0] < 123)
+      s[0] -= 32;
+  }while(s[0] < 65 && s[0] > 90);
+  return s[0];
+}
+
+void init_board_state(void){  /*generate cells with random values (live and dead cells)*/
+  for(int i=0; i<board_h; i++){
+    for(int j=0; j<board_w; j++){
+      board[i][j].current = rand() % 2;
+    }
+  }  
 }
